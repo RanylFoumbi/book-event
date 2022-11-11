c62fdb93d6c6e95f18f2951d59e3ef0cdf28c886
kevin-it237 <ngaleuabel@gmail.com>
2022-11-11 11:53:38 +0100

    fix cors header

--- app.js
+++ app.js
@@ -21,17 +21,18 @@ db.once("open", function () {
 
 
 //allow the app to use json format
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Type, Accept, Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 
 // Set app Routes
